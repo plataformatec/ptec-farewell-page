be45996fee3fdc78365a2b185d68e0565b942025
George Guimarães <george.guimaraes@gmail.com>
2021-01-20 14:32:20 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "Plataformatec Farewell Page",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+  ],
+};

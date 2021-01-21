9d2d9c9febe65aa9f692b4d7aec67d12a7677fb4
George Guimarães <george.guimaraes@gmail.com>
2021-01-20 21:04:25 -0300

    Margins and text

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,39 +20,37 @@ export default function Index() {
           <p>In January 2020, after 11 years delivering high quality projects in Ruby and Elixir to dozens of clients in Brazil, USA, and Europe, Plataformatec has been <a href="https://medium.com/building-nubank/tech-perspectives-behind-nubanks-first-acquisition-deal-what-this-business-move-means-and-how-it-d7d1233c72b8">acqui-hired by Nubank</a>, the first <a href="https://labsnews.com/en/news/business/with-last-funding-nubank-is-the-first-startup-in-brazil-to-be-valued-at-10bi/">decacorn in Brazil</a>.</p>
 
           <p>You can still check the content on our blog at <a href="https://blog.plataformatec.com.br">https://blog.plataformatec.com.br</a>.</p>
-
-
         </div>
       </div>
 
 
-  <div class="relative max-w-7xl mx-auto">
-    <div class="text-center">
-      <h2 class="text-3xl tracking-tight font-extrabold text-ptec-blue sm:text-4xl">
-        What's next?
-      </h2>
-      <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-        Some of the Plataformatec's partners have started new companies. Please check them out!
-      </p>
-    </div>
+      <div class="relative max-w-7xl mx-auto my-16">
+        <div class="text-center">
+          <h2 class="text-3xl tracking-tight font-extrabold text-ptec-blue sm:text-4xl">
+    What's next?
+  </h2>
+  <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+    Some of the Plataformatec's partners have started new companies. Please check them out!
+  </p>
+</div>
 
-    <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-      <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-        <div class="flex-shrink-0">
-              <a href="https://dashbit.co">
-                <img class="h-48 w-full object-contain" src={dashbit_logo} alt="Dashbit Logo"/>
-              </a>
-        </div>
-        <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-          <div class="flex-1">
-            <a href="https://dashbit.co" class="block mt-2">
-              <p class="text-xl font-semibold text-ptec-blue">
-                Dashbit
-              </p>
-              <p class="mt-3 text-base text-gray-500">
+<div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+    <div class="flex-shrink-0">
+      <a href="https://dashbit.co">
+        <img class="h-48 w-full object-contain p-7" src={dashbit_logo} alt="Dashbit Logo"/>
+      </a>
+    </div>
+    <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+      <div class="flex-1">
+        <a href="https://dashbit.co" class="block mt-2">
+          <p class="text-xl font-semibold text-ptec-blue">
+    Dashbit
+  </p>
+  <p class="mt-3 text-base text-gray-500">
     José Valim has openened a new company to help startups and enterprises adopt and run Elixir in production. 
-              </p>
-            </a>
+  </p>
+</a>
           </div>
           <div class="mt-6 flex items-center">
             <div class="flex-shrink-0">
@@ -64,14 +62,14 @@ export default function Index() {
             <div class="ml-3">
               <p class="text-sm font-medium text-gray-900">
                 <a href="https://twitter.com/josevalim" class="hover:underline">
-                  José Valim
-                </a>
-              </p>
-              <div class="flex space-x-1 text-sm text-gray-500">
-                <a href="https://twitter.com/josevalim" class="hover:underline">
-                  @josevalim
-                </a>
-              </div>
+    José Valim
+  </a>
+</p>
+<div class="flex space-x-1 text-sm text-gray-500">
+  <a href="https://twitter.com/josevalim" class="hover:underline">
+    @josevalim
+  </a>
+</div>
             </div>
           </div>
         </div>
@@ -87,32 +85,32 @@ export default function Index() {
           <div class="flex-1">
             <a href="https://sourcelevel.io" class="block mt-2">
               <p class="text-xl font-semibold text-gray-900">
-                SourceLevel
-              </p>
-              <p class="mt-3 text-base text-gray-500">
-                George Guimarães has...
-              </p>
-            </a>
-          </div>
-          <div class="mt-6 flex items-center">
-            <div class="flex-shrink-0">
-              <a href="https://sourcelevel.io">
-                <span class="sr-only">George Guimarães</span>
-                <img class="h-10 w-10 rounded-full" src={georgeguimaraes} alt="George Guimarães picture"/>
-              </a>
-            </div>
-            <div class="ml-3">
-              <p class="text-sm font-medium text-gray-900">
-                <a href="https://twitter.com/georgeguimaraes" class="hover:underline">
-                  George Guimarães
-                </a>
-              </p>
-              <div class="flex space-x-1 text-sm text-gray-500">
-                <a href="https://twitter.com/georgeguimaraes" class="hover:underline">
-                  @georgeguimaraes
-                </a>
+    SourceLevel
+  </p>
+  <p class="mt-3 text-base text-gray-500">
+    George Guimarães has created SourceLevel to bring observability into software development by bringing metrics on process and quality.
+  </p>
+</a>
+  </div>
+  <div class="mt-6 flex items-center">
+    <div class="flex-shrink-0">
+      <a href="https://sourcelevel.io">
+        <span class="sr-only">George Guimarães</span>
+        <img class="h-10 w-10 rounded-full" src={georgeguimaraes} alt="George Guimarães picture"/>
+      </a>
+    </div>
+    <div class="ml-3">
+      <p class="text-sm font-medium text-gray-900">
+        <a href="https://twitter.com/georgeguimaraes" class="hover:underline">
+    George Guimarães               
+  </a>
+</p>
+<div class="flex space-x-1 text-sm text-gray-500">
+  <a href="https://twitter.com/georgeguimaraes" class="hover:underline">
+    @georgeguimaraes
+  </a>
 
-              </div>
+</div>
             </div>
           </div>
         </div>
@@ -122,7 +120,7 @@ export default function Index() {
         <div class="flex-shrink-0">
           <a href="https://www.marcelopark.com.br">
             <img class="h-48 w-full object-cover" src={marcelopark_image} alt=""/>
-            </a>
+          </a>
         </div>
         <div class="flex-1 bg-white p-6 flex flex-col justify-between">
           <div class="flex-1">
@@ -130,12 +128,12 @@ export default function Index() {
             </p>
             <a href="#" class="block mt-2">
               <p class="text-xl font-semibold text-gray-900">
-                Marcelo Park
-              </p>
-              <p class="mt-3 text-base text-gray-500">
+    Marcelo Park
+  </p>
+  <p class="mt-3 text-base text-gray-500">
     Marcelo does consulting and advisory services to cofounders and heads in search for company growth.
-              </p>
-            </a>
+  </p>
+</a>
           </div>
           <div class="mt-6 flex items-center">
             <div class="flex-shrink-0">
@@ -147,9 +145,9 @@ export default function Index() {
             <div class="ml-3">
               <p class="text-sm font-medium text-gray-900">
                 <a href="https://www.marcelopark.com.br/" class="hover:underline">
-                  Marcelo Park
-                </a>
-              </p>
+    Marcelo Park
+  </a>
+</p>
             </div>
           </div>
         </div>
